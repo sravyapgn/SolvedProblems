c416df9477f8fb3d94865ed13cf9d9602417cdad
sgpuliga <sgpuliga@SGPULIGA-7420>
2023-09-29 11:15:26 +0530

    NumberOfSteps Commit

--- ProblemSolved/MathProblems.cs
+++ ProblemSolved/MathProblems.cs
@@ -63,5 +63,29 @@ namespace ProblemSolved
             }
             return res;
         }
+
+        public int NumberOfSteps(int num)
+        {
+            if (num == 0)
+                return 0;
+            if (num == 1)
+                return 1;
+
+
+            int step = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                }
+                else
+                {
+                    num = num - 1;
+                }
+                step++;
+            }
+            return step;
+        }
     }
 }
--- ProblemSolved/Program.cs
+++ ProblemSolved/Program.cs
@@ -4,11 +4,15 @@ using ProblemSolved;
 Console.WriteLine("Hello, World!");
 MathProblems obj = new MathProblems();
 
-Console.WriteLine("Enter number to reverse ");
-Console.WriteLine(obj.Reverse(Convert.ToInt32(Console.ReadLine())));
+//Console.WriteLine("Enter number to reverse ");
+//Console.WriteLine(obj.Reverse(Convert.ToInt32(Console.ReadLine())));
 
-Console.WriteLine("Enter number to reverse ");
-Console.WriteLine(obj.IsPalindrome(Convert.ToInt32(Console.ReadLine())));
+//Console.WriteLine("Enter number to reverse ");
+//Console.WriteLine(obj.IsPalindrome(Convert.ToInt32(Console.ReadLine())));
 
-Console.WriteLine("Given an integer n, return the number of trailing zeroes in n!.");
-Console.WriteLine(obj.TrailingZeroes(Convert.ToInt32(Console.ReadLine())));
+//Console.WriteLine("Given an integer n, return the number of trailing zeroes in n!.");
+//Console.WriteLine(obj.TrailingZeroes(Convert.ToInt32(Console.ReadLine())));
+
+Console.WriteLine("Given an integer num, return the number of steps to reduce it to zero.\r\n\r\nIn one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.");
+Console.WriteLine(obj.NumberOfSteps(Convert.ToInt32(Console.ReadLine())));
+
